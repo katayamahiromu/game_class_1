80353790c89d08f0d4a5bf3265f646a10155bd03
kamabokko0928 <peponi.drop03@gmail.com>
2024-10-20 02:39:42 +0900

    ロード画面関連

--- Source/SceneLoading.h
+++ Source/SceneLoading.h
@@ -22,12 +22,29 @@ public:
 
 	// 描画処理
 	void Render() override;
+
+	//デバッグ
+	void Gui();
 private:
 	//ローディングスレッド
 	static void LoadingThread(SceneLoading* scene);
 private:
 	Sprite* sprite = nullptr;
+	std::unique_ptr<Sprite>pet;
+	std::unique_ptr<Sprite>pet2;
+	std::unique_ptr<Sprite>meter;
+	std::unique_ptr<Sprite>meter2;
 	float angle = 0.0f;
 	Scene* nextScene = nullptr;
 	std::thread* thread = nullptr;
+
+
+	DirectX::XMFLOAT2 m_pos;
+	DirectX::XMFLOAT2 m_texsize;
+	DirectX::XMFLOAT2 m2_pos;
+	DirectX::XMFLOAT2 m2_size;
+	DirectX::XMFLOAT2 m2_texsize;
+
+
+	float alp;
 };
